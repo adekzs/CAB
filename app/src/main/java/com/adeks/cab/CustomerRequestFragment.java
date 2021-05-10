9b0be478fcaf49c5888b5687e53c2b17ea1d8a70
Michael Afolabi <adekuinleafolabi354@gmail.com>
2021-05-10 01:34:29 +0100

    Removed files that are not needed

--- app/src/main/java/com/adeks/cab/CustomerRequestFragment.java
+++ app/src/main/java/com/adeks/cab/CustomerRequestFragment.java
@@ -23,7 +23,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.adeks.cab.dummy.DummyContent;
 import com.adeks.cab.models.Place;
 import com.adeks.cab.models.User;
 import com.google.android.gms.maps.model.LatLng;
